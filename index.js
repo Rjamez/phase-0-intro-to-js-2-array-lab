4bf59890a34e48a735227bce09e09ca93365c365
Rjamez <robin.adhola.student@moringaschool.com>
2024-09-24 09:28:20 +0300

    index.js

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
-// Write your solution here!
+const cats =["Milo","Otis","Garfield"];
+
+function destructivelyAppendCat(name){
+    cats.push("Ralph");
+}
+function destructivelyPrependCat(name){
+    cats.unshift("Bob");
+}
+function destructivelyRemoveFirstCat(name) {
+    cats.shift();
+}
+function destructivelyRemoveLastCat(name) {
+    cats.pop();
+}
+function appendCat(Mike){
+    return[...cats, Mike];
+}
+function prependCat(Jerry) {
+    return [Jerry, ...cats];
+}
+function removeLastCat() {
+    return cats.slice(0, -1);
+}
+function removeFirstCat() {
+    return cats.slice(1);
+}
+
